a48ff26f049cc48d1dfcc2e06eddf4c0c1066886
MrJapa <jjv@live.dk>
2021-07-01 11:33:49 +0200

    Commit

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from tkinter import Button, Tk, HORIZONTAL
 from tkinter.ttk import Progressbar
 from ttkbootstrap import Style
 from tkinter import *
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import selenium
 from selenium.webdriver.common import keys
@@ -25,7 +26,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import *
-from selenium.webdriver.chrome.options import Options
+from os import path
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 options = Options()
@@ -33,39 +34,49 @@ options.headless = True
 driver = webdriver.Chrome(executable_path='chromedriver.exe',options=options)
 driver.get('https://pms.praktikpladsen.dk/soeg-opslag/0/Data-%20og%20kommunikationsuddannelsen/Datatekniker%20med%20speciale%20i%20programmering?aftaleFilter=alle&medarbejdereFilter=alle&adresse=0a3f50ca-f5a5-32b8-e044-0003ba298018&afstand=20')
 
-def website():
-    try:
-        result = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((
-                By.XPATH,'//*[@id="resultater"]/div[1]/div/p')))
-        print(result.text)
-        if path.exists("praktikpladsen.png"):
+class website():
+
+    def __init__(self, website, clickable, path, result, pic, posts):
+        self.website = website
+        self.clickable = clickable
+        self.result = result
+        self.path = path
+        self.pic = pic
+        self.posts = posts
+
+    def search(self):
+        driver.get(self.website)
+        try:
+            result = WebDriverWait(self.website, 10).until(
+                EC.element_to_be_clickable((
+                    By.XPATH,self.clickable)))
+            print(result.text)
+            if path.exists(self.path):
+                pass
+            else:
+                all_ = driver.find_element_by_xpath(self.result)
+                location = all_.location
+                size = all_.size
+                driver.save_screenshot(self.pic)
+            posts = self.posts
+            if result != posts:
+                all_ = driver.find_element_by_xpath(self.result)
+                location = all_.location
+                size = all_.size
+                driver.save_screenshot(self.pic)
+            else:
+                pass
+        except:
             pass
-        else:
-            all_ = driver.find_element_by_xpath('//*[@id="resultater"]')
-            location = all_.location
-            size = all_.size
-            driver.save_screenshot('praktikpladsen.png')
-        posts = '0 stillingsopslag og 35 godkendte virksomheder'
-        if result != posts:
-            all_ = driver.find_element_by_xpath('//*[@id="resultater"]')
-            location = all_.location
-            size = all_.size
-            driver.save_screenshot('praktikpladsen.png')
-        else:
-            pass
-    except:
-        pass
-    driver.quit()
+        driver.quit()
 
 def output_result():
     try:
-        global result
         result = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((
                 By.XPATH,'//*[@id="resultater"]/div[1]/div/p')))
         result = result.text
-        print(result)
+        return result
         driver.quit()
     except:
         pass
@@ -76,7 +87,6 @@ bg = ImageTk.PhotoImage(file='images/background.png')
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 resolution = '{}x{}'.format(screen_width,screen_height)
-print(resolution)
 root.geometry(resolution)
 root.state('zoomed')
 root.title('JapaDash')
@@ -91,7 +101,13 @@ rootcanvas.create_image(0,0,image=bg,anchor='nw')
 logo = PhotoImage(file="images/icon.png")
 logolabel = Label(rootcanvas,image=logo,bd=0,highlightthickness=0).place(x=550,y=350,anchor="center")
 
-ppresult = Label(rootcanvas,text=result).place(x=700,y=400)
+ppresult = Label(rootcanvas,text=output_result()).place(x=700,y=400)
 
-#root.bind("<Configure>", resize_bg)
+if path.exists("images/praktikpladsen.png"):
+    pass
+else:
+    praktikurl = 'https://pms.praktikpladsen.dk/soeg-opslag/0/Data-%20og%20kommunikationsuddannelsen/Datatekniker%20med%20speciale%20i%20programmering?aftaleFilter=alle&medarbejdereFilter=alle&adresse=0a3f50ca-f5a5-32b8-e044-0003ba298018&afstand=20'
+    praktikpladsen = website(praktikurl,'//*[@id="resultater"]/div[1]/div/p','images/praktikpladsen.png','//*[@id="resultater"]','images/praktikpladsen.png','0 stillingsopslag og 35 godkendte virksomheder')
+    praktikpladsen.search()
+driver.quit()
 root.mainloop()
--- test.py
+++ test.py
@@ -1,26 +1,61 @@
+import os
+import tkinter as tk
+from tkinter.constants import ANCHOR
+import tkinter.font as font
+import webbrowser
+import winshell
+import win32com.client
+import keyboard
+import time
+from PIL import ImageTk
+import PIL.Image
+from tkinter import Canvas, PhotoImage, ttk
+from tkinter import messagebox
+from tkinter import Button, Tk, HORIZONTAL
+from tkinter.ttk import Progressbar
+from ttkbootstrap import Style
 from tkinter import *
-from PIL import ImageTk, Image
-root = Tk()
-root.title("Dynamically resize background Image")
-# open image file
-bg = ImageTk.PhotoImage(file="images/background.png")
-# create canvas
-canvas = Canvas(root, width=500, height=500)
-canvas.pack(fill=BOTH, expand=True)
-# place the image inside canvas
-canvas.create_image(0, 0, image=bg, anchor='nw')
-# resize function for resizing the image
-# with proper width and height of root window
-def resize_bg(event):
-    global bgg, resized, bg2
-    # open image to resize it
-    bgg = Image.open("images/background.png")
-    # resize the image with width and height of root
-    resized = bgg.resize((event.width, event.height),
-                         Image.ANTIALIAS)
-    
-    bg2 = ImageTk.PhotoImage(resized)
-    canvas.create_image(0, 0, image=bg2, anchor='nw')
-# bind resized function with root window
-root.bind("<Configure>", resize_bg)
-root.mainloop()
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import selenium
+from selenium.webdriver.common import keys
+from selenium.webdriver.common import by
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import *
+from os import path
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+options = Options()
+options.headless = False
+driver = webdriver.Chrome(executable_path='chromedriver.exe',options=options)
+driver.get('https://pms.praktikpladsen.dk/soeg-opslag/0/Data-%20og%20kommunikationsuddannelsen/Datatekniker%20med%20speciale%20i%20programmering?aftaleFilter=alle&medarbejdereFilter=alle&adresse=0a3f50ca-f5a5-32b8-e044-0003ba298018&afstand=20')
+
+def website():
+    try:
+        result = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((
+                By.XPATH,'//*[@id="resultater"]/div[1]/div/p')))
+        print(result.text)
+        if path.exists("praktikpladsen.png"):
+            pass
+        else:
+            all_ = driver.find_element_by_xpath('//*[@id="resultater"]')
+            location = all_.location
+            size = all_.size
+            driver.save_screenshot('images/praktikpladsen.png')
+        posts = '0 stillingsopslag og 35 godkendte virksomheder'
+        if result != posts:
+            all_ = driver.find_element_by_xpath('//*[@id="resultater"]')
+            location = all_.location
+            size = all_.size
+            driver.save_screenshot('images/praktikpladsen.png')
+        else:
+            pass
+    except:
+        pass
+    driver.quit()
+website()
