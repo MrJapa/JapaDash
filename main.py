f242432cc17aa842e53b870dd0e5c578d14d0805
MrJapa <jjv@live.dk>
2021-07-02 12:49:40 +0200

    Added threading

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import tkinter as tk
 import tkinter.font as font
 import webbrowser
@@ -28,8 +29,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import *
-from os import path
+from os import path, terminal_size
 from webout import output
+import threading
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 options = Options()
@@ -56,50 +58,58 @@ logo = PhotoImage(file="images/icon.png")
 logolabel = Label(rootcanvas,image=logo,bd=0,highlightthickness=0).place(x=350,y=350,anchor="center")
 
 ####!LEAGUE
+def league():
+    lolnew = 'https://euw.leagueoflegends.com/en-us/news/game-updates/'
+    lolnewout = output(lolnew,'//*[@id="gatsby-focus-wrapper"]/div/div[2]/div/div[1]/div/ol/li[1]/a/article/div[2]/div/h2')
+    lolnewresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(lolnewout)).place(x=500,y=450)
+
+    lolurl = 'https://euw.leagueoflegends.com/en-us/news/tags/patch-notes'
+    lolout = output(lolurl,'//*[@id="gatsby-focus-wrapper"]/div/div[2]/div/div[1]/div/ol/li[1]/a/article/div[2]/div/h2')
+    lolresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(lolout)).place(x=500,y=500)
+
 Label(rootcanvas,bg='#1B2222',text="Newest game update:").place(x=500,y=425)
-lolnew = 'https://euw.leagueoflegends.com/en-us/news/game-updates/'
-lolnewout = output(lolnew,'//*[@id="gatsby-focus-wrapper"]/div/div[2]/div/div[1]/div/ol/li[1]/a/article/div[2]/div/h2')
-lolnewresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(lolnewout)).place(x=500,y=450)
-
 Label(rootcanvas,bg='#1B2222',text="Newest League patch:").place(x=500,y=475)
-lolurl = 'https://euw.leagueoflegends.com/en-us/news/tags/patch-notes'
-lolout = output(lolurl,'//*[@id="gatsby-focus-wrapper"]/div/div[2]/div/div[1]/div/ol/li[1]/a/article/div[2]/div/h2')
-lolresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(lolout)).place(x=500,y=500)
-
 lol = PhotoImage(file='images/lol.png')
 lol_button = Button(text="",image=lol,highlightthickness=0,bd=0,command=None)
 lol_canvas = rootcanvas.create_window(500,350,anchor="nw",window=lol_button)
-
 ####!TECHCOLLEGE
+def techcollege():
+    ppurl = 'https://pms.praktikpladsen.dk/soeg-opslag/0/Data-%20og%20kommunikationsuddannelsen/Datatekniker%20med%20speciale%20i%20programmering?aftaleFilter=alle&medarbejdereFilter=alle&adresse=0a3f50ca-f5a5-32b8-e044-0003ba298018&afstand=20'
+    praktikout = output(ppurl,'//*[@id="resultater"]/div[1]/div/p')
+    ppresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(praktikout)).place(x=800,y=450)
+
 Label(rootcanvas,bg='#1B2222',text="Praktikpladsen:").place(x=800,y=425)
-ppurl = 'https://pms.praktikpladsen.dk/soeg-opslag/0/Data-%20og%20kommunikationsuddannelsen/Datatekniker%20med%20speciale%20i%20programmering?aftaleFilter=alle&medarbejdereFilter=alle&adresse=0a3f50ca-f5a5-32b8-e044-0003ba298018&afstand=20'
-praktikout = output(ppurl,'//*[@id="resultater"]/div[1]/div/p')
-ppresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(praktikout)).place(x=800,y=450)
-
-techcollege = PhotoImage(file='images/techcollege.png')
-techcollege_button = Button(text="",image=techcollege,highlightthickness=0,bd=0,Command=None)
+techcollegeimg = PhotoImage(file='images/techcollege.png')
+techcollege_button = Button(text="",image=techcollegeimg,highlightthickness=0,bd=0,command=None)
 techcollege_canvas = rootcanvas.create_window(800,350,anchor="nw",window=techcollege_button)
-
-####!YOUTUBE
-Label(rootcanvas,bg='#1B2222',text="Steam News Hub:").place(x=1100,y=425)
-steamurl = 'https://store.steampowered.com/news/collection/featured/'
-steamout = output(steamurl,'//*[@id="application_root"]/div/div/div[2]/div/div/div[2]/div/div[2]/div/a/div/div/div[1]/div[2]/div[1]/div[1]/div')
-steamresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(steamout)).place(x=1100,y=450)
-
-steamout2 = output(steamurl,'//*[@id="application_root"]/div/div/div[2]/div/div/div[2]/div/div[2]/div/a/div/div/div[1]/div[2]/div[2]')
-steamresult2 = Label(rootcanvas,bg='#1B2222',text=output.output_result(steamout2)).place(x=1100,y=475)
-steam = PhotoImage(file='images/steam.png')
-steam_button = Button(text="",image=steam,highlightthickness=0,bd=0,Command=None)
-steam_canvas = rootcanvas.create_window(1100,350,anchor="nw",window=steam_button)
-
 ####!NYHEDER
-Label(rootcanvas,bg='#1B2222',text="Dagens coronatal:").place(x=500,y=675)
-covidurl = 'https://nyheder.tv2.dk/samfund/dagens-coronatal-overblik-over-smittede-indlagte-og-doede'
-covidout = output(covidurl,'//*[@id="main"]/article/div[2]/div/div/div[2]/div/div[2]/div/table/tbody/tr[1]/th[1]')
-covidresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(covidout)).place(x=500,y=700)
+def nyheder():
+    covidurl = 'https://nyheder.tv2.dk/samfund/dagens-coronatal-overblik-over-smittede-indlagte-og-doede'
+    covidout = output(covidurl,'//*[@id="main"]/article/div[2]/div/div/div[2]/div/div[2]/div/table/tbody/tr[1]/th[1]')
+    covidresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(covidout)).place(x=800,y=700)
 
+Label(rootcanvas,bg='#1B2222',text="Dagens coronatal:").place(x=800,y=675)
 covid = PhotoImage(file="images/covid.png")
 covid_button = Button(text="",image=covid,highlightthickness=0,bd=0,Command=None)
-covid_canvas = rootcanvas.create_window(500,600,anchor="nw",window=covid_button)
+covid_canvas = rootcanvas.create_window(800,600,anchor="nw",window=covid_button)
 
-root.mainloop()
+def opgg():
+    opggurl = 'https://euw.op.gg/summoner/userName=S%CE%B9vas'
+    opggout = output(opggurl, '//*[@id="SummonerLayoutContent"]/div[2]/div[1]/div[1]/div/div[2]')
+    opggresult = Label(rootcanvas,bg='#1B2222',text=output.output_result(opggout)).place(x=500,y=700)
+
+Label(rootcanvas,bg='#1B2222',text="OP.GG Stats:").place(x=500,y=675)
+opggimg = PhotoImage(file="images/opgg.png")
+opgg_button = Button(text="",image=opggimg,highlightthickness=0,bd=0,Command=None)
+opgg_canvas = rootcanvas.create_window(500,600,anchor="nw",window=opgg_button)
+
+def threads():
+    p1 = threading.Thread(target=league)
+    p2 = threading.Thread(target=techcollege)
+    p3 = threading.Thread(target=opgg)
+    p4 = threading.Thread(target=nyheder)
+    p1.start();p2.start();p3.start();p4.start()
+if __name__=='__main__':
+    threads()
+
+    root.mainloop()
